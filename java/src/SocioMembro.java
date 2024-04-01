b474fc675c71529c1abcd22d9fa3be8cdaf5b67a
Fernando <90642301+fernandoprim@users.noreply.github.com>
2024-04-01 13:00:06 -0300

    Update SocioMembro.java

--- java/src/SocioMembro.java
+++ java/src/SocioMembro.java
@@ -2,12 +2,28 @@ public class SocioMembro extends Socio {
 
     private double mensalidade;
 
-    public SocioMembro(String nome, int idade, String CPF, double mensalidade) {
-        super(nome, idade, CPF);
+    public SocioMembro(String nome, int matricula, int idade, String CPF, double mensalidade) {
+        super(nome, matricula, idade, CPF);
+        this.mensalidade = mensalidade;
+    }
+
+    public boolean setMensalidade(double mensalidade){
+        if(mensalidade == 225){
+            this.mensalidade = mensalidade;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getMensalidade(){
+        return this.mensalidade;
     }
 
     @Override
-    public double calculaArrecadacao() {
-        return 0;
+    public String toString(){
+        return super.toString() +
+                "Mensalidade: " + getMensalidade();
     }
+
 }
