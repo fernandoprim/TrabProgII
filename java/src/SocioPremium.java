b182a93b0dcd5b8ed4fae0a505c9e432042e2f00
Fernando <90642301+fernandoprim@users.noreply.github.com>
2024-04-01 13:00:19 -0300

    Update SocioPremium.java

--- java/src/SocioPremium.java
+++ java/src/SocioPremium.java
@@ -2,12 +2,27 @@ public class SocioPremium extends Socio {
 
     private double anuidade;
 
-    public SocioPremium(String nome, int idade, String CPF, double anuidade) {
-        super(nome, idade, CPF);
+    public SocioPremium(String nome, int matricula, int idade, String CPF, double anuidade) {
+        super(nome, matricula, idade, CPF);
+        this.anuidade = anuidade;
+    }
+
+    public boolean setAnuidade(double anuidade){
+        if(anuidade == 980){
+            this.anuidade = anuidade;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getAnuidade(){
+        return this.anuidade;
     }
 
     @Override
-    public double calculaArrecadacao() {
-        return 0;
+    public String toString(){
+        return super.toString() +
+                "Anuidade: " + getAnuidade();
     }
 }
